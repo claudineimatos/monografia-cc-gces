3c8ca66d04b25c7dbd84871dd1d297d281c8ee61
Claudinei Matos <claudineimatos@gmail.com>
2015-04-29 23:48:34 -0300

    * Adicionados patterns e regras para tratamento de mensagens no syslog referente a SSH

--- docker/siem-logstash/startup.sh
+++ docker/siem-logstash/startup.sh
@@ -1,5 +1,7 @@
 #/bin/sh
 
+service postfix start
+
 test "${ELASTICSEARCH_HOST}" != "null" && sed -i "s/#\(.*\)ELASTICSEARCH_HOST\(.*\)/\1${ELASTICSEARCH_HOST}\2/" /opt/logstash/logstash.conf
 
 cd /opt/logstash/
